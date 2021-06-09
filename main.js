e1c5c6f7c22a855b5aeb43ed0839db4f13eb6aef
yoitsro1234 <72605391+yoitsro1234@users.noreply.github.com>
2021-06-09 15:43:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 video = "";
+status = "";
+objects = [];
 
 function preload(){
     video = createVideo("video.mp4");
@@ -7,10 +9,6 @@ function preload(){
 
 function setup(){
     canvas = createCanvas(480, 380);
-}
-
-function draw(){
-    image(video, 0, 0, 480, 380);
 }
 
 function start(){
@@ -24,4 +22,30 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i <= objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects") = "Number of Objects Detected: "+objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label, " " , percent, "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+        }
+    }
 }
